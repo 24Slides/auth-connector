21e5e0d050ece7311c0260b40dcc240498ca3a3f
vinimaks <stayko10@gmail.com>
2020-03-06 18:12:59 +0200

    TFS-635: Mandrill client and request

---
+++ src/Clients/Mandrill/Requests/SendTemplate.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Slides\Connector\Auth\Clients\Mandrill\Requests;
+
+use Illuminate\Support\Arr;
+use Slides\Connector\Auth\Clients\AbstractRequest;
+
+/**
+ * Class SendTemplate
+ *
+ * @property string $template
+ * @property array $recipients
+ * @property array $variables
+ * @property array $from
+ * @property string $subject
+ *
+ * @package Slides\Connector\Auth\Clients\Mandrill\Requests
+ */
+class SendTemplate extends AbstractRequest
+{
+    /**
+     * HTTP method
+     *
+     * @var string
+     */
+    protected $method = 'post';
+
+    /**
+     * Request URI
+     *
+     * @var string
+     */
+    protected $uri = 'messages/send-template.json';
+
+    /**
+     * Validate a response
+     *
+     * @return bool
+     */
+    public function success(): bool
+    {
+        return $this->client->getResponse()->getStatusCode() === 200;
+    }
+
+    /**
+     * Compose a request
+     *
+     * @return void
+     */
+    public function compose()
+    {
+        $optional = [
+            'from_email' => Arr::get($this->from, 'email'),
+            'from_name' => Arr::get($this->from, 'name'),
+            'subject' => $this->subject,
+        ];
+
+        $this->body([
+            'template_name' => $this->template,
+            'template_content' => [],
+            'message' => [
+                    'preserve_recipients' => false,
+                    'track_opens' => true,
+                    'track_clicks' => true,
+                    'to' => $this->recipients,
+                    'merge_vars' => $this->variables,
+                    'merge_language' => 'handlebars'
+                ] + array_filter($optional)
+        ]);
+    }
+}
